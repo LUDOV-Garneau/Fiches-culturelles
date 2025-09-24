d70d0eae2a6d5ece56272495a26f56e30c226c2e
Nour Dahbi <nour.dahbi1@outlook.fr>
2025-09-24 18:32:07 -0400

    change test code for app.js

--- api/app.js
+++ api/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Test API");
+app.get("/api", (req, res) => {
+  res.send("Backend en ligne (route /api)");
 });
 
 app.listen(3000, () => {
