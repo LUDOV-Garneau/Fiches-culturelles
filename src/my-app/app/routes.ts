f78fd97f3a35a0ce2617a7ab7c981f3863320ab1
Nour Dahbi <nour.dahbi1@outlook.fr>
2025-10-06 20:08:51 -0400

    routes path

--- src/my-app/app/routes.ts
+++ src/my-app/app/routes.ts
@@ -1,4 +1,12 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
 export default [index("routes/home.tsx"), 
-    { path: "/casa", file: "routes/casa.tsx" },] satisfies RouteConfig;
+    { path: "/casa", file: "routes/casa.tsx" },
+    { path: "/admin", file: "routes/admin.tsx"},
+    { path: "/login", file: "routes/login.tsx", }
+
+    
+    
+
+
+    ] satisfies RouteConfig;
