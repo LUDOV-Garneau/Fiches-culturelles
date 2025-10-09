89ee7461f0547c4fc1b96e531be69dc064c310bd
dev-t0ny <antoineboudreau@Outlook.com>
2025-10-08 22:29:59 -0400

    feat - routes /jeux & /jeux/:titre

--- api/routes/jeux.js
+++ api/routes/jeux.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { importerJeuxQuebec, listerJeuxQuebec} from "../controllers/jeuController.js";
+import { importerJeuxQuebec, getJeux, getJeu} from "../controllers/jeuController.js";
 
 const router = express.Router();
 
 // Import Koha -> Mongo
 router.get("/import/quebec", importerJeuxQuebec);
-// Lister ceux déjà en Mongo
-router.get("/quebec", listerJeuxQuebec);
+
+// Obtenir tout les jeux
+router.get("/jeux", getJeux);
+
+// Obtenir un jeu précis
+router.get("/jeux/:titre", getJeu);
 
 export default router;
