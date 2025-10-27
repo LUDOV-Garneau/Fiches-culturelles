26e7f4aeb63077e84d69fdb650f1b1ce6c724572
dev-t0ny <antoineboudreau@Outlook.com>
2025-10-27 09:33:53 -0400

    feat - CORS/api

--- api/app.js
+++ api/app.js
@@ -5,6 +5,8 @@ import jeuxRoutes from "./routes/jeux.js";
 
 const app = express();
 
+app.use(cors());
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
