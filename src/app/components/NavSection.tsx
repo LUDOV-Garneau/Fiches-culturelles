9d94032d7795be84b6ed6e9240ff349ceb2ee199
houssem Eddine <houssem_eddine94@hotmail.com>
2025-11-18 11:48:30 -0500

    Ajout des images des liens

--- src/app/components/NavSection.tsx
+++ src/app/components/NavSection.tsx
@@ -1,82 +1,85 @@
-// src/components/NavSection.tsx
 import { Link } from "react-router";
 
-const BAR_HEIGHT = 55;               // hauteur de la barre
-const PRIMARY = "#02cfd8";           // turquoise un peu plus foncé
+import FacebookIcon from "../img/Facebook.png";
+import YouTubeIcon from "../img/youtube.png";
+
+const BAR_HEIGHT = 55;
+const PRIMARY = "#02cfd8";
 
 export function NavSection() {
   return (
     <header
       className="w-full"
       style={{
-        backgroundColor: "transparent", // même fond que ton site -> pas de bande grise foncée
+        backgroundColor: "transparent",
         fontFamily:
           '"Roboto", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif',
         fontSize: "13px",
         fontWeight: 400,
       }}
     >
-      {/* barre alignée à droite comme sur WP */}
       <div
         className="flex items-center justify-end pl-4 pr-16"
         style={{ height: BAR_HEIGHT }}
       >
         <div className="flex items-center gap-10">
-
-          {/* Icônes Facebook + YouTube */}
           <div className="flex items-center gap-8">
 
-            {/* Facebook */}
-            <button
-              type="button"
-              className="flex items-center justify-center"
+            <a
+              href="https://www.facebook.com/LUDOVUdeM/"
+              target="_blank"
+              rel="noopener noreferrer"
               style={{
-                width: 34,
-                height: 34,
-                backgroundColor: PRIMARY,
+                width: 42,
+                height: 42,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 4,
               }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="22"
-                height="22"
-                viewBox="0 0 24 24"
-                fill="#ffffff"
-              >
-                <path d="M13 3h4V0h-4C8.9 0 6 2.9 6 7v3H3v4h3v10h4V14h3l1-4h-4V7c0-1.1.9-2 2-2z" />
-              </svg>
-            </button>
+              <img
+                src={FacebookIcon}
+                alt="Facebook"
+                style={{
+                  width: "42px",
+                  height: "42px",
+                  objectFit: "contain",
+                }}
+              />
+            </a>
 
-            {/* YouTube */}
-            <button
-              type="button"
-              className="flex items-center justify-center"
+            <a
+              href="https://www.youtube.com/channel/UChdKw_7Vr3uKxR271GGBqMw"
+              target="_blank"
+              rel="noopener noreferrer"
               style={{
-                width: 34,
-                height: 34,
-                backgroundColor: PRIMARY,
+                width: 42,
+                height: 42,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 4,
               }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="#ffffff"
-              >
-                <path d="M23.5 6.2c-.3-1.2-1.2-2.1-2.3-2.4C19.3 3.3 12 3.3 12 3.3s-7.3 0-9.2.5C1.7 4.1.8 5 .5 6.2.1 8.1 0 10 0 12s.1 3.9.5 5.8c.3 1.2 1.2 2.1 2.3 2.4 1.9.5 9.2.5 9.2.5s7.3 0 9.2-.5c1.1-.3 2-1.2 2.3-2.4.4-1.9.5-3.8.5-5.8s-.1-3.9-.5-5.8zM9.8 15.6v-7.2L15.8 12l-6 3.6z" />
-              </svg>
-            </button>
+              <img
+                src={YouTubeIcon}
+                alt="YouTube"
+                style={{
+                  width: "42px",
+                  height: "42px",
+                  objectFit: "contain",
+                }}
+              />
+            </a>
           </div>
 
-          {/* Boutons turquoise */}
           <div className="flex items-center gap-4">
-            
             <Link
               to="/contact"
               style={{
                 backgroundColor: PRIMARY,
-                padding: "11px 24px",  // padding EXACT de WordPress
+                padding: "11px 24px",
                 color: "#333333",
                 lineHeight: 1,
                 textDecoration: "none",
@@ -127,7 +130,6 @@ export function NavSection() {
             >
               Login
             </Link>
-
           </div>
         </div>
       </div>
