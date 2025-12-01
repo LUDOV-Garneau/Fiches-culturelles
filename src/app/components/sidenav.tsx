65d094783ebb8b78acd4fd7b737e25985369217a
houssem Eddine <houssem_eddine94@hotmail.com>
2025-12-01 11:16:04 -0500

    Ajout_liens_video

--- src/app/components/sidenav.tsx
+++ src/app/components/sidenav.tsx
@@ -168,14 +168,15 @@ export function Sidenav(){
             </div>
             <div className="sidediv" onMouseEnter={() => setHovered("V")} onMouseLeave={() => setHovered(null)}><p>V</p>  
                   {hovered === "V" && (
-                    <div className="submenu">
-                      <a>Vidéoludique</a>
-                      <ul>
-                        <li><a>Nos événement</a></li>
-                        <li><a>Liens utilies</a></li>
-                        <li><a>Nos partenaires</a></li>
-                      </ul>
-                    </div>
+                  <div className="submenu">
+            <a href="https://www.ludov.ca/fr/videoludiques/" target="_blank">Vidéoludique</a>
+            <ul>
+              <li><a href="https://www.ludov.ca/fr/videoludiques/" target="_blank">Nos événements</a></li>
+              <li><a href="https://www.ludov.ca/fr/videoludiques/" target="_blank">Liens utiles</a></li>
+              <li><a href="https://www.ludov.ca/fr/videoludiques/" target="_blank">Nos partenaires</a></li>
+            </ul>
+          </div>
+
                     )}
             </div>
         </div>
